65a8394431715235cbe0100718b90d3a20067a5f
Sam McClure <samkmcclure@gmail.com>
2018-08-25 12:19:29 -0700

    Add working #show_sequence and #add_random_color

--- W2D1/Simon/lib/simon.rb
+++ W2D1/Simon/lib/simon.rb
@@ -8,31 +8,44 @@ class Simon
   end
 
   def play
-
+    until @game_over
+      take_turn
+    end
+    game_over_message
+    reset_game
   end
 
   def take_turn
-
+    show_sequence
+    require_sequence
   end
 
   def show_sequence
-
+    add_random_color
+    @seq.each do |color|
+      puts "Sequence"
+      puts "#{color}"
+      sleep(1)
+    end
   end
 
   def require_sequence
+    #prompt user for input and check if equal to sequence?
+    user_sequence = []
+
 
   end
 
   def add_random_color
-
+    @seq << COLORS.sample
   end
 
   def round_success_message
-
+    puts "You got it!"
   end
 
   def game_over_message
-
+    puts "Sorry, you lose"
   end
 
   def reset_game
