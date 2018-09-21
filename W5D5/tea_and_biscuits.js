79c3930c4e3d9801bcda941873a91af6cf1b2c59
Sam McClure <samkmcclure@gmail.com>
2018-09-20 21:46:09 -0700

    Add W5D5

---
+++ W5D5/tea_and_biscuits.js
@@ -0,0 +1,31 @@
+const readline = require('readline');
+
+const reader = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+// reader.question("Would you like some tea? ", function (answer) {
+//   console.log(`You said ${answer}`);
+// });
+//
+// reader.question("Would you like some bisuits? ", function (answer){
+//   console.log(`You said ${answer}`);
+// });
+
+function teaAndBiscuits () {
+  reader.question('Would you like some tea?', function (res){
+    console.log(`You said ${res}`);
+    reader.question('Would you like some bisuits?', function (res2) {
+      console.log(`You said ${res2}`);
+
+      const first = (res === 'yes') ? 'do' : 'don\'t';
+      const second = (res2 === 'yes') ? 'do' : 'don\'t';
+
+      console.log(`So you ${first} want tea and you ${second} want biscuits`);
+      reader.close();
+    });
+  });
+}
+
+teaAndBiscuits();
